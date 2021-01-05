4614a75e6c45bd56ae720b8cdc6dc96ad89d7b95
tolentinoel <rckrleyn@gmail.com>
2021-01-04 23:05:53 -0800

    commit tree fc56fa914449cff3c71cfd791aefe8b7294133fa

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
 export function manageFriends(state, action){
+
+    // state ={
+    //     friends: []
+    // }
+
+    switch (action.type) {
+        case 'ADD_FRIEND':
+          return {friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            return {friends: state.friends.filter(friend => friend.id !== action.id)}
+        default:
+          return state;
+      }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,15 @@
 export function managePresents(state, action){
+
+    state = {
+        numberOfPresents: 0
+    }
+
+    switch (action.type) {
+        case 'INCREASE':
+          return {numberOfPresents: state.numberOfPresents + 1}
+        case 'DECREASE':
+          return {numberOfPresents: state.numberOfPresents - 1}
+        default:
+          return state;
+      }
 }
